babddad1d053c07dbb78dc3672e619cca75519dd
grouma <groumanis@gmail.com>
2025-09-28 12:01:43 -0700

    Add Material 3 support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,18 +1,19 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
     namespace = "com.grouma.dateontile"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.grouma.dateontile"
         minSdk = 26
-        targetSdk = 33
-        versionCode = 5
-        versionName = "1.3"
+        targetSdk = 34
+        versionCode = 6
+        versionName = "1.4"
 
     }
 
@@ -42,42 +43,52 @@ android {
 
 dependencies {
 
-    implementation("com.google.android.gms:play-services-wearable:18.1.0")
+    implementation("com.google.android.gms:play-services-wearable:19.0.0")
     // Use to implement support for wear tiles
-    implementation("androidx.wear.tiles:tiles:1.3.0-beta01")
+    implementation("androidx.wear.tiles:tiles:1.5.0")
 
     // Use to utilize standard components and layouts in your tiles
-    implementation("androidx.wear.protolayout:protolayout:1.1.0-beta01")
+    implementation("androidx.wear.protolayout:protolayout:1.3.0")
 
     // Use to utilize components and layouts with Material Design in your tiles
-    implementation("androidx.wear.protolayout:protolayout-material:1.1.0-beta01")
+    implementation("androidx.wear.protolayout:protolayout-material:1.3.0")
+    implementation("androidx.wear.protolayout:protolayout-material3:1.3.0")
 
     // Use to include dynamic expressions in your tiles
-    implementation("androidx.wear.protolayout:protolayout-expression:1.1.0-beta01")
-    implementation("androidx.wear.tiles:tiles-tooling-preview:1.3.0-beta01")
-    implementation("androidx.compose.ui:ui-tooling-preview-android:1.5.4")
+    implementation("androidx.wear.protolayout:protolayout-expression:1.3.0")
 
     // Use to preview wear tiles in your own app
-    debugImplementation("androidx.wear.tiles:tiles-renderer:1.3.0-beta01")
+    debugImplementation("androidx.wear.tiles:tiles-renderer:1.5.0")
 
     // Use to fetch tiles from a tile provider in your tests
-    testImplementation("androidx.wear.tiles:tiles-testing:1.3.0-beta01")
+    testImplementation("androidx.wear.tiles:tiles-testing:1.5.0")
 
-    implementation("com.google.guava:guava:31.0.1-android")
+    // Use to include dynamic expressions in your tiles
+    implementation("androidx.wear.protolayout:protolayout-expression:1.3.0")
+    implementation("androidx.wear.tiles:tiles-tooling-preview:1.5.0")
+    implementation("androidx.compose.ui:ui-tooling-preview-android:1.9.2")
+
+    // Use to preview wear tiles in your own app
+    debugImplementation("androidx.wear.tiles:tiles-renderer:1.5.0")
+
+    // Use to fetch tiles from a tile provider in your tests
+    testImplementation("androidx.wear.tiles:tiles-testing:1.5.0")
+
+    implementation("com.google.guava:guava:33.5.0-jre")
 
     // To use CallbackToFutureAdapter
-    implementation("androidx.concurrent:concurrent-futures:1.1.0")
+    implementation("androidx.concurrent:concurrent-futures:1.3.0")
 
     // Kotlin
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-guava:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-guava:1.10.2")
 
     // Preview Tools
-    implementation("com.google.android.horologist:horologist-compose-tools:0.4.8")
-    implementation("com.google.android.horologist:horologist-tiles:0.4.8")
+    implementation("com.google.android.horologist:horologist-compose-tools:0.7.15")
+    implementation("com.google.android.horologist:horologist-tiles:0.7.15")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("com.google.android.horologist:horologist-compose-tools:0.4.8")
-    implementation("com.google.android.horologist:horologist-tiles:0.4.8")
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    debugImplementation("androidx.compose.ui:ui-tooling:1.5.4")
+    implementation("com.google.android.horologist:horologist-compose-tools:0.7.15")
+    implementation("com.google.android.horologist:horologist-tiles:0.7.15")
+    implementation(platform("androidx.compose:compose-bom:2025.09.01"))
+    debugImplementation("androidx.compose.ui:ui-tooling:1.9.2")
 }
--- app/src/main/java/com/grouma/dateontile/DateTileService.kt
+++ app/src/main/java/com/grouma/dateontile/DateTileService.kt
@@ -1,20 +1,17 @@
 package com.grouma.dateontile
 
 import android.content.Context
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Devices
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.wear.protolayout.ColorBuilders
+import androidx.wear.protolayout.ColorBuilders.argb
 import androidx.wear.protolayout.LayoutElementBuilders
 import androidx.wear.protolayout.TimelineBuilders.Timeline
-import androidx.wear.protolayout.material.Colors
 import androidx.wear.protolayout.material.Text
 import androidx.wear.protolayout.material.Typography
+import androidx.wear.protolayout.material3.ColorScheme
+import androidx.wear.protolayout.material3.materialScope
+import androidx.wear.protolayout.types.LayoutColor
 import androidx.wear.tiles.RequestBuilders
 import androidx.wear.tiles.TileBuilders.Tile
 import androidx.wear.tiles.TileService
-import com.google.android.horologist.compose.tools.LayoutRootPreview
 import com.google.common.util.concurrent.Futures
 import com.google.common.util.concurrent.ListenableFuture
 import java.time.LocalDate
@@ -32,44 +29,33 @@ class DateTileService : TileService() {
         val millisToTomorrow = ChronoUnit.MILLIS.between(now, tomorrow)
 
         return Futures.immediateFuture(
-            Tile.Builder()
-                .setTileTimeline(
-                    Timeline.fromLayoutElement(
-                        tileLayout(context)
-                    )
-                )
-                .setFreshnessIntervalMillis(millisToTomorrow)
-                .build()
+            Tile.Builder().setTileTimeline(
+                Timeline.fromLayoutElement(
+                    materialScope(
+                        this, requestParams.deviceConfiguration, defaultColorScheme = ColorScheme(
+                            primary = LayoutColor(0xFFFFFFFF.toInt()),
+                            secondary = LayoutColor(0xFFBDBDBD.toInt()),
+                        )
+                    ) {
+                        tileLayout(context, colorScheme)
+                    })
+            ).setFreshnessIntervalMillis(millisToTomorrow).build()
         )
     }
 }
 
-private fun tileLayout(context: Context): LayoutElementBuilders.LayoutElement {
+private fun tileLayout(
+    context: Context, colorScheme: ColorScheme
+): LayoutElementBuilders.LayoutElement {
     val date = LocalDate.now()
 
-    return LayoutElementBuilders.Column.Builder()
-        .addContent(
-            Text.Builder(context, date.format(DateTimeFormatter.ofPattern("EEEE")))
-                .setColor(ColorBuilders.argb(Colors.DEFAULT.onSurface))
-                .setTypography(Typography.TYPOGRAPHY_TITLE1)
-                .build()
-        )
-        .addContent(
-            Text.Builder(context, date.format(DateTimeFormatter.ofPattern("MMMM d")))
-                .setColor(ColorBuilders.argb(Colors.DEFAULT.onSurface))
-                .setTypography(Typography.TYPOGRAPHY_TITLE1)
-                .build()
-        )
-        .build()
-}
-
-@Preview(
-    device = Devices.WEAR_OS_SMALL_ROUND,
-    showSystemUi = true,
-    backgroundColor = 0xff000000,
-    showBackground = true
-)
-@Composable
-fun TilePreview() {
-    LayoutRootPreview(root = tileLayout(LocalContext.current))
+    return LayoutElementBuilders.Column.Builder().addContent(
+        Text.Builder(context, date.format(DateTimeFormatter.ofPattern("EEEE")))
+            .setColor(argb(colorScheme.secondary.prop.argb))
+            .setTypography(Typography.TYPOGRAPHY_TITLE1).build()
+    ).addContent(
+        Text.Builder(context, date.format(DateTimeFormatter.ofPattern("MMMM d")))
+            .setColor(argb(colorScheme.primary.prop.argb))
+            .setTypography(Typography.TYPOGRAPHY_TITLE1).build()
+    ).build()
 }
